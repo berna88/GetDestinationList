180f3174e22256461bfb861ff3944e26353a7148
Bernardo <berna_halo@hotmail.com>
2020-01-17 19:25:42 -0600

    update

---
+++ src/main/java/com/consistent/destination/interfaces/Constants.java
@@ -0,0 +1,5 @@
+package com.consistent.destination.interfaces;
+
+public interface Constants {
+
+}
---
+++ src/main/java/com/consistent/destination/portal/Portal.java
@@ -0,0 +1,20 @@
+package com.consistent.destination.portal;
+
+import com.consistent.destination.interfaces.Constants;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+public class Portal implements Constants{
+	
+	private static final Log log = LogFactoryUtil.getLog(Portal.class);
+	
+	/**
+	 * @author bernardohernandez
+	 * @category portal
+	 * @return Devuelve lista de destinos
+	 */
+	protected void getDestination() {
+		
+	}
+}
